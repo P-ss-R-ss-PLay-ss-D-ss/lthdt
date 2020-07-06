0ae97bddd1970b75f3aaab54a6fc09a5e61772f2
baobaote00@gmail.com <baobaote00@gmail.com>
2020-07-06 16:28:25 +0700

    :heart:

--- QLDienThoai/QLDienThoai/Class1.cs
+++ QLDienThoai/QLDienThoai/Class1.cs
@@ -9,7 +9,24 @@ namespace QLDienThoai
     {
         static void Main(string[] args)
         {
-            
+            string file = @"D:\git\lthdt\QLDienThoai\QLDienThoai\Customer.txt";
+        }
+        static String docFile(string file)
+        {
+            StreamReader sr = new StreamReader(file);
+
+            return sr.ReadLine();
+        }
+
+        public void ghiFileDiaChi(string file,string data)
+        {
+            FileStream f = new FileStream(file, FileMode.Open);
+            StreamWriter sr = new StreamWriter(f, Encoding.UTF8);
+
+            sr.Write(data);
+
+            sr.Close();
+            f.Close();
         }
     }
 }
